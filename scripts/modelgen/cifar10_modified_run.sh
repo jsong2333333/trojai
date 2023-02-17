5ac26cfc5b98695e974465ffeb043b54e0c4d669
nsfzyzz <yqyang1991@gmail.com>
2023-02-16 18:19:02 -0800

    nothing

--- scripts/modelgen/cifar10_modified_run.sh
+++ scripts/modelgen/cifar10_modified_run.sh
@@ -1,22 +1,56 @@
-data_folder="/scratch/jialin/trojai/output/cifar10/data"
-experiment_path="/scratch/jialin/trojai/output/cifar10/models"
-models_output="/scratch/jialin/trojai/checkpoints/cifar10/models"
+# random_state:            1   2   3
+#               
+# per_class_trigger_frac:  0.1,  0.2    0.25
+# data_trigger_frac:       0.1   0.2   0.3                        #0.05    0.15   0.25
+# trigger_classes:         0,    4,    9  , 01234 , 0123456789    
 
 
-CUDA_VISIBLE_DEVICES=3 python gen_and_train_cifar10_modified.py \
+
+
+
+#yyaoqing
+# data_folder="/scratch/yyaoqing/trojai/output/cifar10/data"
+# experiment_path="/scratch/yyaoqing/trojai/output/cifar10/models"
+# models_output="/scratch/yyaoqing/trojai/checkpoints/cifar10/models"
+
+
+# CUDA_VISIBLE_DEVICES=3 python gen_and_train_cifar10_modified.py \
+#             $data_folder \
+#             $experiment_path \
+#             --console \
+#             --models_output $models_output \
+#             --tensorboard_dir "/scratch/yyaoqing/trojai/checkpoints/cifar10/tensorboard" \
+#             --early_stopping \
+#             --gpu \
+#             --id 00 \
+#             --random_state 1234 \
+#             --per_class_trigger_frac 0.2 \
+#             --data_trigger_frac 0.3 \
+#             --trigger_classes 1 2 3 4
+
+
+
+
+
+data_folder="/scratch/yyaoqing/trojai/output/cifar10/data"
+experiment_path="/scratch/yyaoqing/trojai/output/cifar10/models"
+models_output="/scratch/yyaoqing/trojai/checkpoints/cifar10/models"
+
+
+CUDA_VISIBLE_DEVICES=3 python gen_and_train_cifar10_modified_jialin.py \
             $data_folder \
             $experiment_path \
             --console \
             --models_output $models_output \
-            --tensorboard_dir "/scratch/jialin/trojai/checkpoints/cifar10/tensorboard" \
-            --early_stopping \
+            --tensorboard_dir "/scratch/yyaoqing/trojai/checkpoints/cifar10/tensorboard" \
             --gpu \
             --id 00 \
             --random_state 1234 \
             --per_class_trigger_frac 0.2 \
             --data_trigger_frac 0.3 \
             --trigger_classes 1 2 3 4
-            
+
+#--early_stopping \
 
 # CUDA_VISIBLE_DEVICES=3 python gen_and_train_mnist.py \
 #         --experiment_path /scratch/yyaoqing/yefan0726/data/trojai/mnist/ \
